02f854ba067ce3b10ba4fd92505d38df7458c9fb
maciol18tv <maicolstivengutierrezardila@gmail.com>
2026-05-16 16:37:25 -0500

    feat: crear formulario de contacto con validacion basica

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Proyecto iniciado");
+console.log("Landing Fitness Pro iniciada");
+
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    alert("Formulario enviado correctamente");
+
+});
