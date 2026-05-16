a267ec707221b9776b24eabff9e0903d9ed7e45b
maciol18tv <maicolstivengutierrezardila@gmail.com>
2026-05-16 16:41:38 -0500

    style: mejorar animaciones y efectos visuales

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ form.addEventListener("submit", (e) => {
     alert("Formulario enviado correctamente");
 
 });
+
+/* EFECTO NAVBAR */
+
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+
+        navbar.style.background = "#111";
+
+    }else{
+
+        navbar.style.background = "#000";
+
+    }
+
+});
